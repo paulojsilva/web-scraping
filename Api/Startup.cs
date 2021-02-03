54151b98c51dca596263c6116758fd36a6e815e2
Paulo Justino <paulo.silva@itixti.com.br>
2021-02-03 01:06:02 -0300

    AddCors

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace Api
 {
@@ -26,6 +21,15 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+               .AddCors(options =>
+               {
+                   options.AddPolicy("CorsPolicy",
+                       builder => builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader());
+               });
+
             services.AddControllers();
             services.Configure(Configuration);
             services.ConfigureAuthentication(Configuration);
@@ -46,6 +50,8 @@ namespace Api
             app.UseAuthentication();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
